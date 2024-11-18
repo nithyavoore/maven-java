0545a5ea39c6be9e8164b713b91b3d38646702d1
nithya <nithyavoore@gmail.com>
2024-11-18 09:55:47 -0600

    maven_java

---
+++ src/main/java/JAVA_MAVEN/W/App.java
@@ -0,0 +1,17 @@
+package JAVA_MAVEN.W;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a=10;
+        int b=9;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
